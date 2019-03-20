2d2aa325e152b5095f0e2e0190701cf1c482b631
unknown <shubhmittal1999@gmail.com>
2019-03-20 18:26:04 +0530

    first_commit

---
+++ equate/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+ int a[5],b[5],i,flag=0;
+ /*equating the values of a and b */
+ printf("enter the values of a\n");
+ for(i=0;i<5;i++)
+ {
+scanf("%d",&a[i]);
+ }
+ printf("enter the values of b\n");
+ for(i=0;i<5;i++)
+ {
+     scanf("%d",&b[i]);
+ }
+ for(i=0;i<5;i++)
+ {
+   if(a[i]!=b[i])
+    {
+    flag=1;
+    break;
+    }
+ }
+ if(flag==1)
+    printf("array are not equal");
+ else
+    printf("array are equal");
+ getch();
+}
